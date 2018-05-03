0427bf612452f2a2f0c863332a8877960f190a36
Martind123 <38527325+Martind123@users.noreply.github.com>
2018-05-03 13:05:37 +0200

    Bugfix v1

--- Platformgame/Assets/Scripts/PlayerScript.cs
+++ Platformgame/Assets/Scripts/PlayerScript.cs
@@ -16,7 +16,7 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKey(KeyCode.Space) && isGrounded) {
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
             myRigidbody.AddForce(Vector3.up * (jumppower * myRigidbody.mass * myRigidbody.gravityScale * 20.0f)); //Vilken riktning man hoppar
         }
 	}
