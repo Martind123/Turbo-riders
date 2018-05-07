8eb1813cde94765dcdb68c5ef644a8cafcdc8286
Martind123 <38527325+Martind123@users.noreply.github.com>
2018-05-07 13:35:59 +0200

    Object movement

---
+++ Platformgame/Assets/Scripts/ChallengeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeController : MonoBehaviour {
+
+    public float scrollerSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Scrolling 
+        GameObject currentChild;
+        for (int i=0; i< transform.childCount; i++) {
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+
+        }
+	}
+
+    void ScrollChallenge (GameObject currentChallenge)
+    {
+        currentChallenge.transform.position -= Vector3.right * (scrollerSpeed * Time.deltaTime);
+
+    }
+}
